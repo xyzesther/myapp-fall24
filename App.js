c58c68aac501fef90391f2bd02fc1c575e199150
xyzesther <zxyesther@gmail.com>
2024-09-12 12:42:35 -0700

    Refactored the Input Component

--- App.js
+++ App.js
@@ -1,25 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
-import { useState } from 'react';
+import Input from './Components/Input';
 
 export default function App() {
   const appName = 'My First React Native App';
-  const [text, setText] = useState('');
 
   return (
     <View style={styles.container}>
       <Header name={appName} />
       <StatusBar style="auto" />
-      <TextInput 
-        placeholder='Type Something Here!' 
-        keyboardType='default' 
-        style={{ borderBottomColor: 'purple', borderBottomWidth: 2 }}
-        value={text}
-        onChangeText={function (changedText) {
-          setText(changedText)
-        }}
-      />
+      <Input />
     </View>
   );
 }
