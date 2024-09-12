8a6315331067770b327382f3ae7af0dbdf226627
xyzesther <zxyesther@gmail.com>
2024-09-12 12:39:17 -0700

    Added a TextInput as a controlled component and a text to show what user enters

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
   const appName = 'My First React Native App';
+  const [text, setText] = useState('');
 
   return (
     <View style={styles.container}>
       <Header name={appName} />
       <StatusBar style="auto" />
+      <TextInput 
+        placeholder='Type Something Here!' 
+        keyboardType='default' 
+        style={{ borderBottomColor: 'purple', borderBottomWidth: 2 }}
+        value={text}
+        onChangeText={function (changedText) {
+          setText(changedText)
+        }}
+      />
     </View>
   );
 }
--- Components/Header.js
+++ Components/Header.js
@@ -1,10 +1,11 @@
 import { StyleSheet, View, Text } from 'react-native'
 import React from 'react'
 
-export default function Header({ name }) {
+export default function Header(props) {
+
   return (
     <View>
-      <Text>Welcome to {name}</Text>
+      <Text>Welcome to {props.name}</Text>
     </View>
   );
 }
