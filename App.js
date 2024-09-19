a01024d7902de6f3637b95f7b9200cf8e394c043
xyzesther <zxyesther@gmail.com>
2024-09-19 12:39:15 -0700

    Refactor layout and styling in App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, SafeAreaView } from 'react-native';
 import Header from './Components/Header';
 import Input from './Components/Input';
 import { useState } from 'react';
@@ -16,22 +16,26 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Header name={appName} />
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Button 
-        title="Add a Goal" 
-        onPress={() => setVisibility(true)} 
-      />
+      <View style={styles.topView}>
+        <Header name={appName} />
+        <Button 
+          title="Add a Goal" 
+          onPress={() => setVisibility(true)} 
+        />
+      </View>
       <Input 
         textInputFocus={true} 
         inputHandler={handleInputData} 
         modalVisible={visibility}
       />
-      <Text style={styles.text}>
-        {receivedData}
-      </Text>
-    </View>
+      <View style={styles.bottomView}>
+        <Text style={styles.text}>
+          {receivedData}
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -39,11 +43,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 
   text: {
     color: "purple",
+    marginVertical: 5,
+  },
+
+  topView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+  },
+
+  bottomView: {
+    flex: 4,
+    backgroundColor: "#dcd",
+    alignItems: 'center',
   },
 });
