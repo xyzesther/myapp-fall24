dda5692fe0cc5adff9f4020982eac7449a079f22
xyzesther <zxyesther@gmail.com>
2024-10-03 12:19:07 -0700

    Added more details button

--- Components/GoalDetails.js
+++ Components/GoalDetails.js
@@ -1,11 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function GoalDetails( navigation, route ) {
   console.log(route)
   return (
     <View>
-      <Text>Details of {route.params.goalObj.text} {route.params.goalObj.id}</Text>
+      {route.params ? (
+        <Text>Details of {route.params.goalObj.text} goal with
+          {route.params.goalObj.id}
+        </Text>
+      ) : (
+        <Text>More Details</Text>
+      )}
+      <Button
+        title="More Details"
+        onPress={() => {
+          navigation.push("Details");
+        }}
+      />
     </View>
   )
 }
