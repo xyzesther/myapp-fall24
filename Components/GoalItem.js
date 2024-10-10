2ed1f332b1f94be13d3f5c4974d1483bcde02f78
xyzesther <zxyesther@gmail.com>
2024-10-10 11:15:45 -0700

    wrapped goalItem in a pressable so we could navigate to goalDetails by tapping on the goal item

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
@@ -6,23 +6,30 @@ export default function GoalItem({ goalObj, handleDelete, handlePress }) {
   const navigation = useNavigation();
   
   return (
-    <View style={styles.textContainer}>
-      <Text style={styles.text}>
-        {goalObj.text}
-      </Text>
-      <Button 
-        title="x" 
-        onPress={() => {
-          handleDelete(goalObj.id);
-        }} color="grey" 
-      />
-
-      <Button 
-        title="i" 
-        onPress={() => {
-          navigation.navigate("Details", { goalObj: goalObj });
-        }} color="grey" 
-      />
+      <View style={styles.textContainer}>
+        <Pressable
+          style={styles.horizontalContainer}
+          onPress={() => {
+            navigation.navigate("Details", { goalObj: goalObj });
+          }}
+        >
+        <Text style={styles.text}>
+          {goalObj.text}
+        </Text>
+        <Button 
+          title="x" 
+          onPress={() => {
+            handleDelete(goalObj.id);
+          }} color="grey" 
+        />
+        {/* <Button 
+          title="i" 
+          onPress={() => {
+            navigation.navigate("Details", { goalObj: goalObj });
+          }} 
+          color="grey" 
+        /> */}
+      </Pressable>
     </View>
   )
 }
@@ -31,6 +38,11 @@ const styles = StyleSheet.create({
   textContainer: {
     backgroundColor: "#ccc",
     borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  horizontalContainer: {
     flexDirection: "row",
     alignItems: "center",
   },
