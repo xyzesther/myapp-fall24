fcd10c923b186d9fb00c36e32f1b357df78a75f5
xyzesther <zxyesther@gmail.com>
2024-11-14 12:00:27 -0800

    Refactor LocationManager to use foreground permissions and improve error handling

--- Components/LocationManager.js
+++ Components/LocationManager.js
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import * as Location from 'expo-location';
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+// import { getCurrentPositionAsync } from "expo-location";
+import * as Location from "expo-location";
 
 export default function LocationManager() {
   const [location, setLocation] = useState(null);
-  const [response, requestPermission] = Location.useLocationPermissions();
-  
+  const [response, requestPermission] = Location.useForegroundPermissions();
   async function verifyPermission() {
     //check if user has granted permission return true
     try {
@@ -20,32 +20,29 @@ export default function LocationManager() {
       console.log("verify permission ", err);
     }
   }
-
   async function locateUserHandler() {
     try {
-      // call verify permission and only proceed to open camera if you have permission
+      // call verifyPermission and only proceed if you have permission
       const hasPermission = await verifyPermission();
       if (!hasPermission) {
-        Alert.alert("You need to give location permission");
+        Alert.alert("YOu need to give location permission");
         return;
       }
       const locationResponse = await Location.getCurrentPositionAsync();
       console.log(locationResponse);
       setLocation({
-        latitude: locationResponse.coords.latitude, 
-        longitude: locationResponse.coords.longitude
+        latitude: locationResponse.coords.latitude,
+        longitude: locationResponse.coords.longitude,
       });
-    }
-    catch (err) {
-      console.log("Location error", err);
+    } catch (err) {
+      console.log("locate user ", err);
     }
   }
-
   return (
     <View>
       <Button title="Locate Me" onPress={locateUserHandler} />
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
